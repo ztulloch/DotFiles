7222979c8653ab5eef4afc31335e338bafb7efa9
ztulloch <ztulloch@gmail.com>
2018-08-07 12:00:29 +0100

    Fixes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# setup emacs
 cp dot.emacs.lin ~/.emacs 
-mkdir ~/EmacsRoot
-cp universal-init.el ~/EmacsRoot
+if [ ! -d ~/.emacs.d ]; then
+    mkdir ~/EmacsRoot
+fi
+mkdir ~/.emacs.d/site-lisp
+mkdir ~/.emacs.d/lisp
+cp universal-init.el ~/.emacs.d/site-lisp
+
+# setup bash
 cat bashrc >> ~/.bashrc
 cp bash_machine ~/.bash_machine
