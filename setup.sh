e159609c3d0cf5d8897917dfc13b462fede1e833
ztulloch <ztulloch@gmail.com>
2018-08-13 19:33:01 +0100

    Changed EmacsRoot to .emacs.d/

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # setup emacs
 cp dot.emacs.lin ~/.emacs 
 if [ ! -d ~/.emacs.d ]; then
-    mkdir ~/EmacsRoot
+    mkdir ~/.emacs.d
 fi
 mkdir ~/.emacs.d/site-lisp
 mkdir ~/.emacs.d/lisp
@@ -11,3 +11,9 @@ cp universal-init.el ~/.emacs.d/site-lisp
 # setup bash
 cat bashrc >> ~/.bashrc
 cp bash_machine ~/.bash_machine
+sed -i -- 's/username/ztulloch/g' pinerc
+sed -i -- 's/mailserver/tulltech/g' pinerc
+
+cp pinerc ~/.pinerc
+cp pine_passfule ~/.pine_passfile
+
